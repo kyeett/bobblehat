b2c39550f70130bc6e35331fbee6d0925ccabbbf
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2021-05-16 22:18:32 +0200

    wip

--- examples/test/main.go
+++ examples/test/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kyeett/bobblehat/sense/screen"
 	"github.com/kyeett/bobblehat/sense/screen/color"
 	"time"
 )
 
 func main() {
+	fmt.Println("starting")
 	// create a new frame buffer
 	fb := screen.NewFrameBuffer()
 
@@ -14,17 +16,12 @@ func main() {
 	fb.SetPixel(0, 0, color.Red)
 	fb.SetPixel(1, 1, color.Red)
 
-	for i := uint16(0); i < 65535; i++ {
-		//t := screen.FrameBuffer{
-		//	Texture: &texture.Texture{
-		//		Pixels: []color.Color{color.Color(i), color.Color(i), color.Color(i)},
-		//	},
-		//}
-		//screen.Draw(&t)
-		screen.DrawAny(i)
+	fmt.Println("red")
+	for i := uint16(0); i < 256; i++ {
+		draw(uint8(i), 0, 0)
 		time.Sleep(100 * time.Millisecond)
 	}
-
+	
 	//f, err := os.Create(backBuffer)
 	//if err != nil {
 	//	return err
@@ -32,4 +29,9 @@ func main() {
 	//defer f.Close()
 	//
 	//return binary.Write(f, binary.LittleEndian, fb.Texture.Pixels)
+}
+
+func draw(red, green, blue uint8) {
+	r := red >> 3
+	screen.DrawAny(r << 11)
 }
